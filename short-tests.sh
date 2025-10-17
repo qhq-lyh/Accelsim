414e4b857111480d3300ac9685327b8ceaeab16c
qhq-lyh <1365327835@qq.com>
2025-10-17 15:14:00 +0800

    Adjust Short-test.sh

--- short-tests.sh
+++ short-tests.sh
@@ -20,7 +20,35 @@ tar -xzvf rodinia_2.0-ft.tgz -C ./hw_run
 rm rodinia_2.0-ft.tgz
 
 #Run the tests on the trace
-./util/job_launching/run_simulations.py -C QV100-SASS -B rodinia_2.0-ft -T ./hw_run/rodinia_2.0-ft/9.1 -N myTest
+./util/job_launching/run_simulations.py -C QV100-SASS -B rodinia_2.0-ft -T ./hw_run/rodinia_2.0-ft/9.1 -N Accelwattch_Test
 
-# Wait for them to finish
-./util/job_launching/monitor_func_test.py -v -N myTest
+# Wait for them to finish , monitor this simulation
+./util/job_launching/monitor_func_test.py -v -N Accelwattch_Test
+
+# Collect the stats
+./util/job_launching/get_stats.py -N myTest | tee per-app-fromlaunch.csv # divide in app
+./util/job_launching/get_stats.py -K -k -N myTest | tee per-kernel-instance.csv # divide in kernel for each app
+
+./util/job_launching/get_stats.py -R -B rodinia_2.0-ft -C QV100-SASS | tee per-app-for-autoplot.csv # get stats for plot
+./util/job_launching/get_stats.py -K -k -R -B rodinia_2.0-ft -C QV100-SASS | tee per-app-for-correlation.csv # get stats for correlation analysis
+
+
+# Accelwattch(A Power Modeling Framework for Modern GPUs) tests
+./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C QV100-Accelwattch_SASS_SIM -T ./hw_run/rodinia_2.0-ft/9.1 -N Accelwattch_Test
+
+# Collect the stats
+./util/job_launching/get_stats.py -N Accelwattch_Test | tee accelwattch-per-app-fromlaunch.csv
+./util/job_launching/get_stats.py -K -k -N Accelwattch_Test | tee accelwattch-per-kernel-instance.csv
+
+# Accelwattch HW tests
+./util/job_launching/run_simulations.py -B rodinia_2.0-ft -a -C QV100-Accelwattch_SASS_HW -T ./hw_run/rodinia_2.0-ft/9.1 -N AccelwattchHW_Test # require corresponding hw_perf.csv
+
+# run your own traces on your device
+./util/tracer_nvbit/install_nvbit.sh
+make -C ./util/tracer_nvbit/
+git clone https://github.com/accel-sim/gpu-app-collection
+source ./gpu-app-collection/src/setup_environment
+make -j -C ./gpu-app-collection/src rodinia_2.0-ft
+make -C ./gpu-app-collection/src data
+
+./util/tracer_nvbit/run_hw_trace.py -B rodinia_2.0-ft -D 0 # traces result in hw_run/traces/device-0/<cuda-version>
